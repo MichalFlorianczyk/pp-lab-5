96a121acd6b76cfbb9209795ec37063ca7740e44
MichalFlorianczyk <166236193+MichalFlorianczyk@users.noreply.github.com>
2024-05-04 19:32:21 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 import company.implementations.EmailMessenger;
-import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Hello, this is an email message!");
+        int result = MathUtils.add(10, 20);
+        System.out.println("Sum of numbers: " + result);
+
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("The sum of 10 and 20 is: " + result);
     }
 }
